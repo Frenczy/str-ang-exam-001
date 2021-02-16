65b588c78e390592b4537348e89c342594106bb8
Frenczy <frenczypp@gmail.com>
2021-02-16 11:55:43 +0100

    Commit changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 import { FilterPipe } from '../pipe/filter.pipe';
+import { SorterPipe } from '../pipe/sorter.pipe';
 import { FormsModule } from '@angular/forms'
 
 
@@ -13,8 +14,12 @@ import { FormsModule } from '@angular/forms'
 export class HeroesComponent implements OnInit {
 
   phrase:string=""
-
+  order: any=""
   herolist=this.hs.getAll()
+  sortbyID(event:Event){this.order="id"}
+  sortbyName(event:Event){this.order="name"}
+  sortbySuperPower(event:Event){this.order="superPower"}
+  sortbyAddress(event:Event){this.order="address"}
 
   constructor(private hs:HeroService) { }
 
